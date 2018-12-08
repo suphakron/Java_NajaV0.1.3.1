e818a5ed99abf0c708d7d66eb707f075bf97dbf3
suphakron <thebankks@gmail.com>
2018-12-09 04:56:03 +0700

    update Certificate

---
+++ app/src/main/java/com/javanajautc/theba/java_naja/CertificateActivity.java
@@ -0,0 +1,74 @@
+package com.javanajautc.theba.java_naja;
+
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+
+public class CertificateActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    FirebaseUser user;
+    TextView username;
+    DatabaseReference current_user_db, current_user_db_fName, current_user_db_lName;
+    private String Fname, Lname, user_id, imgLocation;
+    DatabaseReference myRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_certificate);
+
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        myRef = FirebaseDatabase.getInstance().getReference();
+
+        user_id = user.getUid();
+        current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
+
+        username = findViewById(R.id.Cert_username);
+
+        current_user_db_fName = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id).child("FName");
+        current_user_db_fName.addValueEventListener(new ValueEventListener() {
+            //final ProgressDialog progressDialog = ProgressDialog.show(CertificateActivity .this, "Please wait...", "Loading...", true, true);
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Fname = dataSnapshot.getValue(String.class);
+                //progressDialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        current_user_db_lName = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id).child("LName");
+        current_user_db_lName.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                final ProgressDialog progressDialog = ProgressDialog.show(CertificateActivity .this, "Please wait...", "Loading...", true, true);
+                Lname = dataSnapshot.getValue(String.class);
+                username.setText(Fname + " " + Lname);
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+}
--- app/src/test/java/com/example/theba/java_naja/ExampleUnitTest.java
+++ app/src/test/java/com/example/theba/java_naja/ExampleUnitTest.java
@@ -1,15 +1,15 @@
-package com.example.theba.java_naja;
+package com.javanajautc.theba.java_naja;
 
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * javanajautc local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class javanajautcUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
